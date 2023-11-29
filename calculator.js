949f14bd14e6bc0e5ee2c2785ef9b792092bb9d8
cjshallow <cjshallow@gmail.com>
2023-11-28 20:43:10 -0500

    Created Basic HTML Calculator with Buttons and Initial Functions

---
+++ calculator.js
@@ -0,0 +1,29 @@
+const add = function(a,b) {
+    return a + b 
+      
+  };
+  
+  const subtract = function(a,b) {
+    return a - b 
+      
+  };
+
+const multiply = function(a,b) {
+    return a * b
+};
+
+const divide = function(a,b) {
+    return a / b 
+};
+
+const operate = function(a, operation, b) {
+    if (operation === '+') return add(a,b); 
+    if (operation === '-') return subtract(a,b); 
+    if (operation === '*') return multiply(a,b); 
+    if (operation === '/') return b === 0 ? "ERROR" : divide(a,b);
+};
+
+function displayText() {
+  let x = document.getElementById("buttonDisplay").ariaValueMax;
+  document.getElementById('display').innerHTML = x;
+}
