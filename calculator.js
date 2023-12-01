c8757a0714c3913207fe407e67941ab301815bfe
cjshallow <cjshallow@gmail.com>
2023-12-01 00:25:37 -0500

    Functionality to Print Text Content of Number Buttons to Display

--- calculator.js
+++ calculator.js
@@ -1,5 +1,43 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperationScreen = document.getElementById('previousOperandScreen')
+const currentOperandScreen = document.getElementById('currentOperandScreen')
 
 
+numberButtons.forEach(button => {
+   button.addEventListener('click', () => {
+      appendNumber(button.textContent)
+   })
+})
+
+function clear() {
+
+}
+
+
+function deleteNumber() {
+
+
+}
+
+function appendNumber(number) {
+   currentOperandScreen.textContent += number
+}
+
+function chooseOperation(operation) {
+
+}
+
+
+function compute() {
+
+}
+
+function updateDisplay() {
+}
 
 
 const add = function(a,b) {
