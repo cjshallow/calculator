28d6d1ec8580708fbad740e60ffe104b3d6d39a1
cjshallow <cjshallow@gmail.com>
2023-11-30 23:59:08 -0500

    Restart for Learning Purposes

--- calculator.js
+++ calculator.js
@@ -1,16 +1,5 @@
 
 
-// Psuedo Code // 
-
-// Create function so that when the Equals Button is clicked, the 
-// operate function runs with the two numbers input into the display
-// and prints the solution into the display
-
-// store the first number, then the operator, then the second number into variables
-// make those variables the arguments of the operate function
-// run the operate function when equals is clicked 
-// add something to the operate function to print the result into the display
-
 
 
 const add = function(a,b) {
