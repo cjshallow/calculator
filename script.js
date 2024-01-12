518d022ed62481044e6bf9faeec644e6434f2fc2
cjshallow <cjshallow@gmail.com>
2024-01-12 10:43:04 -0500

    Both Screens Functioning with AC DEL and Decimal Buttons Working

--- script.js
+++ script.js
@@ -18,20 +18,20 @@ clearButton.addEventListener('click', clear)
 deleteButton.addEventListener('click', deleteValue)
 decimalButton.addEventListener('click', appendDecimal)
 
-function compute(){
-
-}
 
 function clear(){
+  currentOperationScreen.textContent = ""
+  previousOperationScreen.textContent = ""
 
 }
 
 function deleteValue() {
-
+  currentOperationScreen.textContent = currentOperationScreen.textContent.toString().slice(0,-1)
 }
 
 function appendDecimal() {
-
+  if (currentOperationScreen.textContent.includes(".")) return
+ currentOperationScreen.textContent += "."
 }
 
 
@@ -39,6 +39,64 @@ numberButtons.forEach((button) =>
   button.addEventListener('click', () => appendNumber(button.textContent))
 )
 
+operatorButtons.forEach((button) =>
+  button.addEventListener('click', () => setOperator(button.textContent))
+)
+
 function appendNumber(number) {
     currentOperationScreen.textContent += number
+}
+
+function setOperator(operator){
+  currentValue = currentOperationScreen.textContent
+  currentOperation = operator
+  previousOperationScreen.textContent = `${currentValue} ${operator}`
+  currentOperationScreen.textContent = ""
+}
+
+
+
+const add = function(a,b) {
+  return Number(a) + Number(b) 
+	
+};
+
+const subtract = function(a,b) {
+  return a - b 
+	
+};
+
+const sum = function(array) {
+  if(array.length === 0) {
+    return 0
+  } else {
+    let arrSum = array.reduce((sum, current) => sum + current);
+    return arrSum;
+  }
+};
+
+const multiply = function(a,b) {
+  return a*b 
+  
+};
+
+const divide =  function(a,b) {
+  if (b === 0) {
+    return "ERROR";
+  } else {
+    return a / b
+  };
+
+}
+
+function compute(){
+  if (previousOperationScreen.textContent.includes("+" || "-" || "*" || "÷")){
+    if (previousOperationScreen.textContent.includes("+")){
+      b = currentOperationScreen.textContent
+      a = currentValue
+      solution = 
+      currentOperationScreen.textContent = solution 
+      add()
+    } 
+  } 
 }
